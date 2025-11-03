0fc6c0fa3dabebfbd88c8b5dec32e0d72b81e3b8
Ahmed F. Al-Refaie <ahmed.f.alrefaie@gmail.com>
2025-11-03 13:01:30 +0000

    Rename to /api

--- apps/web/lib/api/client/client.gen.ts
+++ apps/web/lib/api/client/client.gen.ts
@@ -13,8 +13,8 @@ import type { ClientOptions as ClientOptions2 } from './types.gen';
  */
 export type CreateClientConfig<T extends ClientOptions = ClientOptions2> = (override?: Config<ClientOptions & T>) => Config<Required<ClientOptions> & T>;
 
-export const BASE_URL: string = '/backend';
-export const BACKEND_URL: string = '/backend';
+export const BASE_URL: string = '/api';
+export const BACKEND_URL: string = '/api';
 
 export const client = createClient(createConfig<ClientOptions2>({
     baseUrl: BASE_URL,
