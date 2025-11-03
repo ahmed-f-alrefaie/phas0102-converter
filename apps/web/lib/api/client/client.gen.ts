f2522a60ae581e2443b7cd8ead94cf648f288152
Ahmed F. Al-Refaie <ahmed.f.alrefaie@gmail.com>
2025-11-03 13:11:31 +0000

    updated baseurl

--- apps/web/lib/api/client/client.gen.ts
+++ apps/web/lib/api/client/client.gen.ts
@@ -13,8 +13,8 @@ import type { ClientOptions as ClientOptions2 } from './types.gen';
  */
 export type CreateClientConfig<T extends ClientOptions = ClientOptions2> = (override?: Config<ClientOptions & T>) => Config<Required<ClientOptions> & T>;
 
-export const BASE_URL: string = '/api';
-export const BACKEND_URL: string = '/api';
+export const BASE_URL: string = '/';
+export const BACKEND_URL: string = '/';
 
 export const client = createClient(createConfig<ClientOptions2>({
     baseUrl: BASE_URL,
